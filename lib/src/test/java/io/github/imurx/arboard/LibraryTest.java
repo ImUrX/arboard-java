f04557bcfbbae0e5eec4630b27a45361aa1f6196
ImUrX <urielfontan2002@gmail.com>
2022-01-24 20:36:55 -0300

    new test

--- lib/src/test/java/io/github/imurx/arboard/LibraryTest.java
+++ lib/src/test/java/io/github/imurx/arboard/LibraryTest.java
@@ -13,4 +13,22 @@ public class LibraryTest {
             e.printStackTrace();
         }
     }
+
+    private static final byte[] image = {127,127,127,127,
+        127,127,127,127,
+    };
+    @Test
+    public void setImage() {
+        try (Clipboard clip = new Clipboard()) {
+            ImageData data = new ImageData(2, 1, image);
+            clip.setImage(data);
+            Thread.sleep(5000);
+            ImageData data2 = clip.getImage();
+            assertArrayEquals(image, data2.getImage());
+            data.close();
+            data2.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
